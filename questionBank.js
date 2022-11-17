b859b650704a43ddb534acb3e5db6fe807f42758
Jpitt <Jaredpittman0@gmail.com>
2022-11-17 11:22:01 -0330

    Terraria Questions filled

--- questionBank.js
+++ questionBank.js
@@ -1,15 +1,54 @@
 question_bank = {
 
-    "T1": ["", "", "", ""],
-    "T2": ["", "", "", ""],
-    "T3": ["", "", "", ""],
-    "T4": ["", "", "", ""],
-    "T5": ["", "", "", ""],
-    "T6": ["", "", "", ""],
-    "T7": ["", "", "", ""],
-    "T8": ["", "", "", ""],
-    "T9": ["", "", "", ""],
-    "T10": ["", "", "", ""],
+    "T1": ["Wood Armor",
+            "Shadow Armor",
+             "Gold Armor",
+             "Copper Armor"],
+
+    "T2": ["Corruption",
+            "Forest",
+            "Hell",
+            "Meteor"],
+
+    "T3": ["Iron Ore",
+            "Obsidian",
+            "Vile Mushroom",
+            "Stone Block"],
+
+    "T4": ["Eater of Worlds",
+            "Skeletron", 
+            "Wall of Flesh",
+            "Ender Dragon"],
+
+    "T5": ["Gills Potion",
+            "Wrath Potion",
+            "Iron Skin Potion",
+            "Obsidian Skin Potion"],
+
+    "T6": ["Gold Pickaxe",
+            "Nightmare Pickaxe",
+            "Molten Pickaxe",
+            "Silver Pickaxe"],
+
+    "T7": ["King Slime, Eye of Cthulhu, Eater of World",
+            "Moon Lord, King Slime, Brain of Cthulhu",
+            "Queen Bee, Empress of Light, Destroyer",
+            "Skeletron Prime, The Twins, Plantrea"],
+
+    "T8": ["Dark Lance",
+            "Ghastly Glaive",
+            "Daybreak",
+            "Chlorophyte Partisan"],
+
+    "T9": ["Tim",
+            "Goblin Scout",
+            "Jungle Slime",
+            "Undead Miner"],
+
+    "T10": ["4",
+            "12",
+            "7",
+            "15"],
 
     "M1" : ["", "", "", ""],
     "M1" : ["", "", "", ""],
