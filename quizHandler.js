d67ccc9c3f994aa7e6a4c2e554c259221e0bb605
Jpitt <Jaredpittman0@gmail.com>
2022-11-17 13:11:03 -0330

    random functions

---
+++ quizHandler.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    function getRandomTerraria(){
+        return Math.floor(Math.random() * 10);
+    }
+    function getRandomMinecraft(){
+        return  Math.floor(Math.random() * 20) + 10;
+    }
+    
+    console.log(getRandomMinecraft);
+    console.log(getRandomTerraria);
+})
