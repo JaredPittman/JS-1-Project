0022c15fd89efa2d813b130754fcc0a407cf2d64
Jpitt <Jaredpittman0@gmail.com>
2022-11-17 11:31:27 -0330

    Index changed for terraria quesitons

--- questionBank.js
+++ questionBank.js
@@ -1,51 +1,51 @@
 question_bank = {
 
-    "T1": ["Wood Armor",
+    1: ["Wood Armor",
             "Shadow Armor",
              "Gold Armor",
              "Copper Armor"],
 
-    "T2": ["Corruption",
+    2 : ["Corruption",
             "Forest",
             "Hell",
             "Meteor"],
 
-    "T3": ["Iron Ore",
+    3: ["Iron Ore",
             "Obsidian",
             "Vile Mushroom",
             "Stone Block"],
 
-    "T4": ["Eater of Worlds",
+    4: ["Eater of Worlds",
             "Skeletron", 
             "Wall of Flesh",
             "Ender Dragon"],
 
-    "T5": ["Gills Potion",
+    5: ["Gills Potion",
             "Wrath Potion",
             "Iron Skin Potion",
             "Obsidian Skin Potion"],
 
-    "T6": ["Gold Pickaxe",
+    6: ["Gold Pickaxe",
             "Nightmare Pickaxe",
             "Molten Pickaxe",
             "Silver Pickaxe"],
 
-    "T7": ["King Slime, Eye of Cthulhu, Eater of World",
+    7: ["King Slime, Eye of Cthulhu, Eater of World",
             "Moon Lord, King Slime, Brain of Cthulhu",
             "Queen Bee, Empress of Light, Destroyer",
             "Skeletron Prime, The Twins, Plantrea"],
 
-    "T8": ["Dark Lance",
+    8: ["Dark Lance",
             "Ghastly Glaive",
             "Daybreak",
             "Chlorophyte Partisan"],
 
-    "T9": ["Tim",
+    9: ["Tim",
             "Goblin Scout",
             "Jungle Slime",
             "Undead Miner"],
 
-    "T10": ["4",
+    10: ["4",
             "12",
             "7",
             "15"],
